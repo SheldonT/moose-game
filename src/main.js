90cfca6f74d59262aab4866a66af65729448a947
SheldonT <towersheldon@gmail.com>
2023-12-28 06:03:04 -0330

    Experimenting wit Electron

---
+++ src/main.js
@@ -0,0 +1,28 @@
+/** @format */
+
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
